fd7d8264796c9135753faa399e37b95a2589e10b
Dmitri So <dmitriso@yahoo.com>
2021-01-06 22:11:39 -0500

    Establishing connection to employeecms_db through mysql.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
+//CONNECTING TO THE DATABASE
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Dmso0929',
+  database: 'playlist_db',
+});
 
 
 
