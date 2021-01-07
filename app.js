ee309071350063f42fd6475353425c2184f91287
Dmitri So <dmitriso@yahoo.com>
2021-01-07 15:41:17 -0500

    Created Inilialize function for first prompt/Using switch case statement to call CRUD functions.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 
 //CONNECTING TO THE DATABASE
 const connection = mysql.createConnection({
@@ -16,4 +15,44 @@ connection.connect((err) => {
   console.log('connected as id ' + connection.threadId);
   connection.end();
 });
+
+init();
+//INQUIRER PROMPT TO GENERATE QUESTIONS IN THE CLI
+function init() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'start',
+      message: 'What would you like to do?',
+      choices: ['View Employees', 'View Roles', 'View Department', 'Add Employee', 'Add Role', 'Add Department', 'Update Employee Role','Exit']
+    }
+  ]).then((data) => {
+    switch (data.start) {
+      case 'Add Employee':
+        addEmployee();
+        break;
+      case 'Add Role':
+        addRole();
+        break;
+      case 'Add Department':
+        addDepartment();
+        break;
+      case 'View Employees':
+        viewEmployees();
+        break;
+      case 'View Roles':
+      viewroles();
+        break;
+      case 'View Department':
+        viewDepartment();
+        break;
+      case 'Update Employee Role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        connection.end();
+        break;
+    }
+  })
+}
 
